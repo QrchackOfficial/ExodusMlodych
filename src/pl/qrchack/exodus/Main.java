538721e673c0ab491148e795310990e930afcb4a
QrchackOfficial <qrchackofficial@gmail.com>
2013-12-30 00:18:13 +0100

    Dial buttons working

--- src/pl/qrchack/exodus/Main.java
+++ src/pl/qrchack/exodus/Main.java
@@ -11,6 +11,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.*;
 import android.content.Context;
+import android.app.*;
+import java.net.*;
 
 public class Main extends Activity {
     WebView webView;
@@ -22,20 +24,22 @@ public class Main extends Activity {
         setContentView(R.layout.main);
         webView = (WebView)findViewById(R.id.fullscreen_content);
         webView.setWebViewClient(new MyWebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                webView.loadUrl("file:///android_asset/www/error.html");
-            }
-			public void onLoadResource(WebView view, String url) {
+			public void onPageFinished(WebView view, String url) {
 				if(url.startsWith("tel:")) {
 					// Open dialer intent
-					Toast.makeText(getApplicationContext(), "TODO: Implement dialer intent here", 1000).show();
+					// Toast.makeText(view.getContext(), "TODO: Implement dialer intent here", 1000).show();
+					Intent dial = new Intent(Intent.ACTION_DIAL);
+					dial.setData(Uri.parse(url));
+					startActivity(dial);
 					view.goBack();
 				}
 				if(url.startsWith("mailto:")) {
-					Toast.makeText(getApplicationContext(), "TODO: Implement email intent here", 1000).show();
-					view.goBack();
+					Toast.makeText(view.getContext(), "TODO: Implement email intent here", 1000).show();
 				}
 			}
+            public void onReceivedError(WebView view, int errorCode, String description, String url) {
+				webView.loadUrl("file:///android_asset/www/error.html");
+            }
         });
         webView.loadUrl("file:///android_asset/www/index.html");
         //you can also link to a website. Example:
