d4dbf8c4626b1766cc95d1013281ab705cb54e0f
QrchackOfficial <qrchackofficial@gmail.com>
2013-12-30 15:20:28 +0100

    [WIP] removing unnecessary class

--- src/pl/qrchack/exodus/Main.java
+++ src/pl/qrchack/exodus/Main.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.*;
 import android.content.Context;
 import android.app.*;
@@ -23,7 +24,7 @@ public class Main extends Activity {
 
         setContentView(R.layout.main);
         webView = (WebView)findViewById(R.id.fullscreen_content);
-        webView.setWebViewClient(new MyWebViewClient() {
+        webView.setWebViewClient(new WebViewClient() {
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				if(url.startsWith("tel:")) {
 					Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
