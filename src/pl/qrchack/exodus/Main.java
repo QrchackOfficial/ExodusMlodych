1d99ee7f05141147c10b55bce9a5791e93284557
julianvolodia <julianvolodia@gmail.com>
2014-07-31 11:38:21 +0200

    Code cleanup

--- src/pl/qrchack/exodus/Main.java
+++ src/pl/qrchack/exodus/Main.java
@@ -1,7 +1,7 @@
 package pl.qrchack.exodus;
 
+// Imports
 import java.io.IOException;
-
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
@@ -11,44 +11,35 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-// import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-
 public class Main extends Activity {
     WebView webView;
     private MediaPlayer mp;
     AssetFileDescriptor afd;
-    public static String prevUrl;/*
-    public boolean IsPlaying;
-    public boolean IsDifferent;
-    public int choose;*/
+    public static String prevUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        mp = new MediaPlayer();
-        
-        /*IsPlaying = false;
-        IsDifferent = false;
-        choose = 0;*/
-        
         setContentView(R.layout.main);
+        mp = new MediaPlayer();
+
         webView = (WebView)findViewById(R.id.fullscreen_content);
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
 
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {	
-				if(url.startsWith("tel:")) {
+
+				if (url.startsWith("tel:")) {
 					Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
 					startActivity(dial);
 					return true;
 				}
-				if(url.startsWith("mailto:")) {
+				if (url.startsWith("mailto:")) {
 					Intent intent = new Intent(Intent.ACTION_SENDTO);
 					intent.setType("text/plain");
 					/*
@@ -56,126 +47,75 @@ public class Main extends Activity {
 					intent.putExtra(Intent.EXTRA_TEXT, "Body of email");
 					*/
 					intent.setData(Uri.parse(url));
-					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 					startActivity(intent);
+
 					return true;
 				}
-				if(url.startsWith("fb:")) {
-					try
-					{
+				if (url.startsWith("fb:")) {
+					try {
 						getApplicationContext().getPackageManager().getPackageInfo("com.facebook.katana", 0);
 						Intent fb = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/"+url.substring(3)));
 						startActivity(fb);
 					}
-					catch (PackageManager.NameNotFoundException e)
-					{
+					catch (PackageManager.NameNotFoundException e) {
 						Intent fb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id="+url.substring(3)));
 						startActivity(fb);
 					}
+
 					return true;
 				}
-				if(url.startsWith("yt:")) {
+				if (url.startsWith("yt:")) {
 					Intent intent = null;
 					try {
         				intent = new Intent(Intent.ACTION_VIEW);
         				intent.setPackage("com.google.android.youtube");
      					intent.setData(Uri.parse(url.substring(3)));
         				startActivity(intent);
-    				} catch (ActivityNotFoundException e) {
+    				}
+    				catch (ActivityNotFoundException e) {
         				intent = new Intent(Intent.ACTION_VIEW);
        					intent.setData(Uri.parse(url.substring(3)));
         				startActivity(intent);
     				}
+
 					return true;
 				}
 		        if (url.endsWith(".mp3") || url.endsWith(".ogg")) {
-
 		        	try {
-			        	url = url.replace("file:///android_asset/", ""); /// :D xD thumb up ;P
+			        	url = url.replace("file:///android_asset/", "");
 						afd = getAssets().openFd(url);
 						
 						//Toast.makeText(getApplicationContext(), "choose: "+choose + " " + prevUrl + " " + url, Toast.LENGTH_LONG).show();
 						
-						if( url.equals(Main.prevUrl)){
-							Toast.makeText(getApplicationContext(), "te same", Toast.LENGTH_LONG).show();
+						if (url.equals(Main.prevUrl)) {
+							// Toast.makeText(getApplicationContext(), "te same", Toast.LENGTH_LONG).show();
 							mp.stop();
 							Main.prevUrl="";
 						}
-						else 
-						{
-								Toast.makeText(getApplicationContext(), "inne", Toast.LENGTH_LONG).show();
-								if (mp.isPlaying())
-									mp.stop();
+						else {
+							// Toast.makeText(getApplicationContext(), "inne", Toast.LENGTH_LONG).show();
+							if (mp.isPlaying()) mp.stop();
 	
 				        	Main.prevUrl=url;
-								
 							mp.reset();
 		        			mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
 			        		mp.prepare();
 			        		mp.start();
-						
-						}
-/*
-						if(prevUrl != url)
-						{   
-							IsDifferent = true;
-							if(choose<2)choose +=2;
-						}else
-						{
-							IsDifferent = false;
-							if(choose>1)choose -=2;
-						}
-
-						prevUrl = url;
-						
-						switch(choose)
-						{
-							case 1: // IsPlaying
-								mp.stop();
-								IsPlaying = false;
-			        			choose -= 1;
-								break;
-							case 3: // IsPlaying & IsDifferent
-								mp.stop();
-								
-								IsPlaying = false;
-			        			choose -= 1;
-								prevUrl = url;
-			        			
-			        			mp.reset();		
-			        			mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-				        		mp.prepare();
-				        		mp.start();
-				        		break;
-							case 2: // IsDifferent ...
-								IsPlaying = true;
-			        			choose += 1;
-			        			mp.reset();
-								mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-				        		mp.prepare();
-				        		mp.start();
-								break;
-							case 0: // ... or the same BUT NOT PLAYING
-			        			IsPlaying = true;
-			        			choose += 1;
-			        			mp.reset();
-			        			mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-				        		mp.prepare();
-				        		mp.start();
-								break;
-								default: throw new Exception();
-							}*/
-						
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
+			        	}
+					}
+					catch (IOException e) {
+						/
+						/ TODO Auto-generated catch block
 						Toast.makeText(getApplicationContext(), "IOException: "+e.getMessage(), Toast.LENGTH_LONG).show();
 						e.printStackTrace();
 					
-					} catch (Exception e)
-					{
+					}
+					catch (Exception e) {
 						Toast.makeText(getApplicationContext(), "Exception: "+e.getMessage(), Toast.LENGTH_LONG).show();
 						e.printStackTrace();
 					}
+
 		            return true;
 		        }
 				else {
@@ -188,9 +128,6 @@ public class Main extends Activity {
             }
         });
         webView.loadUrl("file:///android_asset/www/index.html");
-        //you can also link to a website. Example:
-        //webView.loadUrl("www.google.com");
-        //I have included web permissions in the AndroidManifest.xml
         webView.setWebChromeClient(new WebChromeClient());
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -204,17 +141,12 @@ public class Main extends Activity {
     @Override
     public void onBackPressed()
     {
-        if(webView.canGoBack())
-            webView.goBack();
-        else
-            super.onBackPressed();
+        if(webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
     
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
     }
-
-    
 }
